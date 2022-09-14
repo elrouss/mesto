1c179b662a1e23524c5da6ff31fb0841854a7598
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2022-09-14 23:27:35 +0300

    feat: реализовать загрузку галереи через JS, остановиться на попапах

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,47 @@
 'use strict'
 
-// Открытие и закрытие модального окна редактирования информации в профиле, со значениями со страницы
-const popupEditProfileInfo = document.querySelector('.popup');
-const openPopupEditProfileInfo = document.querySelector('.profile__edit-button');
-const closePopupEditProfileInfo = document.querySelector('.popup__close-button');
+// МОДАЛЬНЫЕ ОКНА
+// Открытие и закрытие модальных окон
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
+const popupClosingButton = document.querySelector('.popup__closing-button');
+
+const popup = document.querySelectorAll('.popup');
+const popupEditingProfileInfo = document.querySelector('.popup_type_edit-profile');
+const closingButtonPopupEditProfileInfo = document.querySelector('.popup__closing-button_type_edit-profile');
+const popupAddingPhotocard = document.querySelector('.popup_type_add-photocard');
+const closingButtonPopupAddPhotocard = document.querySelector('.popup__closing-button_type_add-photocard');
+
+const togglePopup = (popup) => {
+  popup.classList.toggle('popup_opened');
+};
 
 const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector ('.profile__job');
+const profileJob = document.querySelector('.profile__job');
 const nameInput = document.editProfileInfo.profileName;
 const jobInput = document.editProfileInfo.profileJob;
 
-
-const togglePopupEditProfileInfo = () => {
-  popupEditProfileInfo.classList.toggle('popup_opened');
-}
-
-function transferProfileInfo () {
+const transferProfileInfo = () => {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-}
+};
 
-openPopupEditProfileInfo.addEventListener('click', () => {
-  togglePopupEditProfileInfo();
+profileEditButton.addEventListener('click', () => {
+  togglePopup(popupEditingProfileInfo);
   transferProfileInfo();
-})
+});
 
-closePopupEditProfileInfo.addEventListener('click', () => {
-  togglePopupEditProfileInfo();
-})
+popupClosingButton.addEventListener('click', () => {
+togglePopup(closingButtonPopupEditProfileInfo);
+});
+
+profileAddButton.addEventListener('click', () => {
+  togglePopup(popupAddingPhotocard);
+});
+
+popupClosingButton.addEventListener('click', () => {
+  togglePopup(closingButtonPopupAddPhotocard);
+  });
 
 // Редактирование информации профиля в модальном окне с сохранением значений
 // и автоматическим закрытием окна после успешной отправки данных
@@ -39,7 +53,54 @@ function formSubmitHandler (evt) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  togglePopupEditProfileInfo();
-}
+  togglePopup(popupEditingProfileInfo);
+};
 
 formEl.addEventListener('submit', formSubmitHandler);
+
+// ГАЛЕРЕЯ ФОТОКАРТОЧЕК
+const photoGallery = document.querySelector('.gallery');
+const photocardTemplate = document.querySelector('#gallery-template').content;
+
+const initialPhotocards = [
+  {
+    name: 'Фаллох',
+    link: 'https://images.unsplash.com/photo-1609109238958-eb5130c99873?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2051&q=80',
+    alt: 'Водопад Фаллох в Шотландии'
+  },
+  {
+    name: 'Чинкве-Терре',
+    link: 'https://images.unsplash.com/photo-1592396355679-1e2a094e8bf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    alt: 'Национальный парк "Чинкве-Терре" ("Пять земель") в Италии'
+  },
+  {
+    name: 'Мыс Нельсон',
+    link: 'https://images.unsplash.com/photo-1434873740857-1bc5653afda8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=778&q=80',
+    alt: 'Маяк на мысе Нельсон в Австралии'
+  },
+  {
+    name: 'Агра',
+    link: 'https://images.unsplash.com/photo-1604917549272-558e388753a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+    alt: 'Тадж-Махал в Агре'
+  },
+  {
+    name: 'Венеция',
+    link: 'https://images.unsplash.com/photo-1534113414509-0eec2bfb493f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    alt: 'Канал в Венеции'
+  },
+  {
+    name: 'Абу-Даби',
+    link: 'https://images.unsplash.com/photo-1532370436137-d9aaea5dab36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    alt: 'Дворец Каср Аль-Ватан в Абу-Даби'
+  }
+];
+
+initialPhotocards.forEach((item) => {
+  const photocardElement = photocardTemplate.cloneNode(true);
+
+  photocardElement.querySelector('.gallery__item-image').src = item.link; // Обращение по селектору выполняется дважды
+  photocardElement.querySelector('.gallery__item-image').alt = item.alt;  // Нужно вынести в отдельную константу
+  photocardElement.querySelector('.gallery__item-title').textContent = item.name;
+
+  photoGallery.append(photocardElement);
+});
